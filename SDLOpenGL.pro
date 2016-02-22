cd5c0a5c1956b63328fcfda69b61cde6ee8595e1
mwoods <i7694562@bournemouth.ac.uk>
2016-02-22 11:55:50 +0000

    added readme

--- SDLOpenGL.pro
+++ SDLOpenGL.pro
@@ -1,9 +1,13 @@
 TARGET=SDLOpenGL
 
 OBJECTS_DIR=obj
-SOURCES+=$$PWD/src/main.cpp
-         $$PWD/src/SDLOpenGL.cpp
+INCLUDEPATH+=$$PWD/include
+
+SOURCES+=$$PWD/src/main.cpp \
+         $$PWD/src/SDLOpenGl.cpp
 
 HEADERS+=$$PWD/include/SDLOpenGl.h
-
+QMAKE_CXXFLAGS += $$system(sdl2-config --cflags)
+LIBS+=$$system(sdl2-config --libs)
 include($(HOME)/NGL/UseNGL.pri)
+OTHER_FILES += README.md
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,29 @@
 # include <iostream>
+#include "SDLOpenGl.h"
+#include <ngl/NGLInit.h>
+
+void initalizeGL();
+void renderGL();
 
 int main (int argc, char *argv[])
 {
-  std::cout<<"working\n";
+  SDLOpenGL window("test", 0, 0, 1024, 720);
+  initalizeGL();
+  while(true)
+  {
+    renderGL();
+    window.swapWindow();
+    SDL_Delay(10);
+  }
+}
+
+void initalizeGL()
+{
+  ngl::NGLInit::instance();
+  glClearColor(0.8, 0.8, 0.8, 1.0);
+}
+
+void renderGL()
+{
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
